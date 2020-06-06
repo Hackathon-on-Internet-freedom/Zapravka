ca31fe3e30daaf928b8114379e53984caaa6c917
krutmaster <krutmaster25@mail.ru>
2020-06-07 02:16:09 +0300

    C:/Program Files/Git/asnwer, /agree, /refuse, listletters added

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ def createConfig():
     config.set('List users', 'count', '0')
     letters = {}
     config.set('List users', 'letters', str(letters))
+    wait_users = {}
+    config.set('List users', 'wait_users', str(wait_users))
 
     with open('config.ini', 'w') as config_file:
         config.write(config_file)
@@ -35,6 +37,7 @@ owner_id = config.get('Settings', 'owner_id')
 admin_id = config.get('Settings', 'admin_id')
 count = int(config.get('List users', 'count'))
 letters = str_in_dict(config.get('List users', 'letters'))
+wait_users = str_in_dict(config.get('List users', 'wait_users'))
 bot = telebot.TeleBot(token)
 
 
@@ -43,10 +46,10 @@ def start(message):
     if owner_id != str(message.chat.id) and str(message.chat.id) != admin_id:
         bot.send_message(message.chat.id, 'Здравствуйте. Этот бот принимает жалобы на нарушение прав и свобод '
                                           'человека.\nКак пользоваться? Всё просто: отправляйте подряд текстовые '
-                                          'сообщения, фото, видео, документы, которые составят ваше письмо '
+                                          'сообщения, фото, видео, документы, аудио, которые составят ваше письмо '
                                           'администрации. После того, как всё отправите, нажмите на копку "send"'
                                           ' или отправьте команду /send . Вы получите индивидуальный номер письма, '
-                                          'а само письмо отправится администрации. ')
+                                          'а само письмо отправится администрации.')
 
 
 @bot.message_handler(commands=["setgroupowner"])
@@ -101,16 +104,20 @@ def listorders(message):
         bot.send_message(admin_id, '-------Список неотвеченных писем-------')
 
         for letter in letters:
-            bot.send_message(admin_id, f'Заказ №{letter} - {letters[letter]}')
+            bot.send_message(admin_id, f'Заказ №{letter}')
+
+            if letters[letter] in wait_users:
+                bot.send_message(admin_id, 'Отправитель этого письма не ответил на отправленный вопрос')
 
         bot.send_message(admin_id, '---------------------------------------')
 
 
 @bot.message_handler(commands=['send'])
 def send(message):
-    global config, count
+    global config, count, wait_users
 
-    if str(message.chat.id) != owner_id and str(message.chat.id) != admin_id:
+    if str(message.chat.id) != owner_id and str(message.chat.id) != admin_id\
+            and not str(message.chat.id) in wait_users:
         bot.reply_to(message, 'Письмо отправляется, ждите')
 
         try:
@@ -139,6 +146,33 @@ def send(message):
         except Exception:
             bot.reply_to(message, 'Ничего нет к отправке')
 
+    elif str(message.chat.id) in wait_users:
+        bot.reply_to(message, 'Ответ отправляется, ждите')
+
+        try:
+            files = os.listdir(f'temp/{message.chat.id}')
+            bot.send_message(owner_id, '--------------')
+            bot.send_message(owner_id, f'Дополение к письму №{wait_users[str(message.chat.id)]}')
+
+            for name in files:
+                with open(f'temp/{message.chat.id}/{name}', 'rb') as file:
+                    bot.send_document(owner_id, file)
+
+                os.remove(f'temp/{message.chat.id}/{name}')
+
+            bot.send_message(owner_id, '--------------')
+            os.rmdir(f'temp/{message.chat.id}')
+            letters[count] = str(message.chat.id)
+            del wait_users[str(message.chat.id)]
+            config.set('List users', 'wait_users', str(wait_users))
+
+            with open('config.ini', 'w') as config_file:
+                config.write(config_file)
+
+            bot.reply_to(message, f'Ваш ответ успешно отправлен')
+        except Exception:
+            bot.reply_to(message, 'Ничего нет к отправке')
+
 
 @bot.message_handler(commands=['answer'])
 def answer(message):
@@ -147,10 +181,65 @@ def answer(message):
 
         try:
             letter = int(message.text.split()[1])
-            answer = message.text.split()[2]
+            answer = str(message.text.split()[2:]).replace('[', '').replace(']', '').replace(',', '').replace("'", '')
+            wait_users[letters[letter]] = letter
+            config.set('List users', 'wait_users', str(wait_users))
+
+            with open('config.ini', 'w') as config_file:
+                config.write(config_file)
+
             bot.send_message(letters[letter], f'Ответ администрации: {answer}')
             bot.reply_to(message, f'Ответ отправлен автору письма {letter}')
+        except Exception:
+            bot.reply_to(message, 'Такого письма нет или команда введена неправильно')
 
+
+@bot.message_handler(commands=['agree'])
+def agree(message):
+
+    if str(message.chat.id) == admin_id:
+
+        try:
+            letter = int(message.text.split()[1])
+            bot.send_message(letters[letter], f'Ваше письмо №{letter} одобренно администрацией,'
+                                              ' следите за каналом @zapravka38\nСпасибо за помощь!')
+            del wait_users[letters[letter]]
+            del letters[letter]
+            config.set('List users', 'letters', str(letters))
+            config.set('List users', 'wait_users', str(wait_users))
+
+            with open('config.ini', 'w') as config_file:
+                config.write(config_file)
+
+            bot.reply_to(message, f'Отправитель письма №{letter} получил уведомление о согласии'
+                                  f' работать с его письмом. Письмо удалено из списка неотвеченных')
+        except Exception:
+            bot.reply_to(message, 'Такого письма нет или команда введена неправильно')
+
+
+@bot.message_handler(commands=['refuse'])
+def refuse(message):
+    if str(message.chat.id) == admin_id:
+
+        try:
+            letter = int(message.text.split()[1])
+            bot.send_message(letters[letter], f'Ваше письмо №{letter} не прошло модерацию и не будет рассмотрено'
+                                              ' на канале @zapravka38\n')
+
+            try:
+                del wait_users[letters[letter]]
+            except Exception:
+                pass
+
+            del letters[letter]
+            config.set('List users', 'letters', str(letters))
+            config.set('List users', 'wait_users', str(wait_users))
+
+            with open('config.ini', 'w') as config_file:
+                config.write(config_file)
+
+            bot.reply_to(message, f'Отправитель письма №{letter} получил уведомление об отказе'
+                                  f' работать с его письмом. Письмо удалено из списка неотвеченных')
         except Exception:
             bot.reply_to(message, 'Такого письма нет или команда введена неправильно')
 
@@ -207,13 +296,13 @@ def handle_photo(message):
     if str(message.chat.id) != owner_id and str(message.chat.id) != admin_id:
 
         try:
-            file_info = bot.get_file(message.photo[2].file_id)
+            file_info = bot.get_file(message.photo[1].file_id)
             downloaded_file = bot.download_file(file_info.file_path)
 
             if not os.path.exists(f'temp/{message.chat.id}'):
                 os.mkdir(f'temp/{message.chat.id}')
 
-            with open(f'temp/{message.chat.id}/{message.photo[2].file_id}.jpg', 'wb') as new_file:
+            with open(f'temp/{message.chat.id}/{message.photo[1].file_id}.jpg', 'wb') as new_file:
                 new_file.write(downloaded_file)
 
             bot.reply_to(message, 'Фото сохранёно. Если отправлены все файлы, нажмите кнопку "send"'
